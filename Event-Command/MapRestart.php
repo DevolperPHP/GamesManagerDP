b2cdd534cddf286d244247b217e3219bef43e481
DevolperPHP <devolperphp@gmail.com>
2017-08-05 16:48:51 +0300

    Update MapRestart.php

--- Event-Command/MapRestart.php
+++ Event-Command/MapRestart.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace GamesManagerDP/Event-Command/MapRestart;
+
+use EDLB\GameSender;
+
+class ResetMap{
+  
+  public function __construct(GameSender $plugin){
+    
+		$this->plugin = $plugin;
+    
+	}
+  
+  public function resetWorld($){
+    $name = self::WORLD;
+    $this->getServer()->unloadLevel($this->getServer()->getLevelByName($name));
+    $zip = new /ZipArchive;
+    $zip->open($this->getServer()->getDataFolder."Map/".$name.".zip");
+    $zip->extractTo($this->getServer()->getDataPath()."worlds");
+    $zip->close;
+    unset($zip);
+    $this->getServer()->loadLevel(self::WORLD);
+}
